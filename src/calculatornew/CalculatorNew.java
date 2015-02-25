b0c8f62c48123069a5386fc313aa122fb56926ad
ap <ap@EDS-1.eds.local>
2015-02-25 15:05:18 +0300

    Optimized code Add comments

--- src/calculatornew/CalculatorNew.java
+++ src/calculatornew/CalculatorNew.java
@@ -10,6 +10,11 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Simple Calculator. Only basic operations. One of firsts my projects.
+ *
+ * @author mandry
+ */
 public class CalculatorNew {
 
     private final JPanel pnlButtons2;
@@ -22,34 +27,50 @@ public class CalculatorNew {
     private boolean start = true;
     private final ActionE btnResult;
 
+    /**
+     * main void. Create object of class CalculatorNew
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         CalculatorNew calc = new CalculatorNew();
 
     }
 
+    /**
+     * Constructor
+     */
     public CalculatorNew() {
 
+        //create frame 300x300
         JFrame mainFrame = new JFrame("Калькулятор");
         mainFrame.setSize(300, 300);
         mainFrame.setResizable(false);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // main panel with Border layout
         JPanel mainPanel = new JPanel(new BorderLayout(3, 3));
+
+        //clear button where will be shown result of calculate
         lblResult = new JButton("");
         lblResult.setActionCommand("clear");
         lblResult.addActionListener(new ActionE());
         lblResult.setFont(new Font("Segoe UI", 3, 20));
-
         lblResult.setPreferredSize(new Dimension(300, 50));
         mainPanel.add(lblResult, BorderLayout.NORTH);
 
+        //panel with digit buttons
         pnlButtons = new JPanel(new GridLayout(4, 3, 5, 5));
+        //panel with action buttons
         pnlButtons2 = new JPanel(new GridLayout(4, 1, 5, 5));
 
+        //action listeners for buttons
         btnAction = new ActionE();
         btnDigit = new ActionE();
         btnResult = new ActionE();
+
+        //create buttons with names and actions
         for (int i = 1; i < 11; i++) {
             addButton(String.valueOf(i % 10), btnDigit);
         }
@@ -60,9 +81,11 @@ public class CalculatorNew {
         addButton("*", btnAction);
         addButton("/", btnAction);
 
+        //add buttons to panels
         mainPanel.add(pnlButtons, BorderLayout.CENTER);
         mainPanel.add(pnlButtons2, BorderLayout.EAST);
 
+        //add main panels to frame
         mainFrame.add(mainPanel);
         mainFrame.setVisible(true);
     }
@@ -72,16 +95,15 @@ public class CalculatorNew {
         JButton btn = new JButton(name);
         btn.setFont(new Font("Segoe UI", 1, 20));
         btn.addActionListener(action);
-        
+
         if (action == btnDigit) {
             btn.setActionCommand("digit");
             pnlButtons.add(btn);
         } else if (action == btnAction) {
             btn.setActionCommand("action");
             pnlButtons2.add(btn);
-
         } else if (action == btnResult) {
-            btn.setActionCommand("result");
+            btn.setActionCommand("action");
             pnlButtons.add(btn);
         }
 
@@ -90,75 +112,63 @@ public class CalculatorNew {
     class ActionE implements ActionListener {
 
         @Override
-        @SuppressWarnings("empty-statement")
         public void actionPerformed(ActionEvent e) {
 
             JButton btn = (JButton) e.getSource();
 
-            if (btn.getActionCommand().equals("clear")) {
-                lblResult.setText("");
-                start = true;
+            switch (btn.getActionCommand()) {
+                case "clear":
+                    lblResult.setText("");
+                    start = true;
+                    a = 0;
+                    b = 0;
+                    znak = "+";
+                    break;
+                case "digit":
+                    lblResult.setText(lblResult.getText() + btn.getText());
+                    break;
+                case "action":
 
-            }
-            if (start) {
-                switch (e.getActionCommand()) {
-                    case "digit":
-                        lblResult.setText(lblResult.getText() + btn.getText());
-                        break;
-                    case "action":
-                        if (lblResult.getText().isEmpty() || lblResult.getText().equals(".")) {
-                            a = 0.0;
+                    if (lblResult.getText().isEmpty() || lblResult.getText().equals(".")) {
+                        if (start) {
+                            a = 0;
+                            start = false;
                         } else {
-                            a = Double.parseDouble(lblResult.getText());
+                            b = 0;
+                            lblResult.setText(calculate(a, b, znak));
+                            start = true;
                         }
-                        ;
-                        b = 0.0;
-                        lblResult.setText("");
-                        start = false;
-                        znak = btn.getText();
-                        break;
-                }
-
-            } else {
-                switch (e.getActionCommand()) {
-                    case "digit":
-                        lblResult.setText(lblResult.getText() + btn.getText());
-                        break;
-                    case "action":
-                        znak = btn.getText();
-                        lblResult.setText("");
-                        break;
-                    case "result":
-                        if (lblResult.getText().isEmpty() || lblResult.getText().equals(".")) {
-                            b = 0.0;
+                    } else {
+                        if (start) {
+                            a = Double.parseDouble(lblResult.getText());
+                            znak = btn.getText();
+                            lblResult.setText("");
+                            start = false;
                         } else {
                             b = Double.parseDouble(lblResult.getText());
+                            lblResult.setText(calculate(a, b, znak));
+                            start = true;
                         }
-                        ;
-                        lblResult.setText(calculate(a, b, znak));
-                        start = true;
-                        a = 0.0;
-                        b = 0.0;
-                        znak = "+";
-                        break;
-                }
+                    }
 
+                    break;
             }
-        }
 
-        private String calculate(double a, double b, String znak) {
-
-            switch (znak) {
-                case "+":
-                    return String.valueOf(a + b);
-                case "-":
-                    return String.valueOf(a - b);
-                case "*":
-                    return String.valueOf(a * b);
-                case "/":
-                    return String.valueOf(a / b);
-            }
-            return null;
         }
+    }
+
+    private String calculate(double a, double b, String znak) {
+
+        switch (znak) {
+            case "+":
+                return String.valueOf(a + b);
+            case "-":
+                return String.valueOf(a - b);
+            case "*":
+                return String.valueOf(a * b);
+            case "/":
+                return String.valueOf(a / b);
+        }
+        return null;
     }
 }
